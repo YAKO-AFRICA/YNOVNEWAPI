2931db50d700213e20f9b74ff41a9edf609e8858
bruceyapo <bruceyapo2000@gmail.com>
2026-08-25 23:14:19 +0000

    Update reset password

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-<?php
-// routes/web.php
-
-use Illuminate\Support\Facades\Response;
-use Illuminate\Support\Facades\Route;
-
 Route::get('storage/documents/{file}', function ($file) {
     // Nettoyer le nom du fichier
     $file = ltrim($file, '/');
